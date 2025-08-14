e8841bdc8e72c1a5ac552c2ef521ac60c3516a73
Walikrence <969327526@qq.com>
2025-08-14 15:12:52 +0800

    lab1练习5代码

--- labcodes/lab1/kern/debug/kdebug.c
+++ labcodes/lab1/kern/debug/kdebug.c
@@ -295,25 +295,40 @@ void print_stackframe(void) {
    * funciton's return addr eip  = ss:[ebp+4] the calling funciton's ebp =
    * ss:[ebp]
    */
-  uint32_t ebp = read_ebp();
-  uint32_t eip = read_eip();
+  // uint32_t ebp = read_ebp();
+  // uint32_t eip = read_eip();
+  // int i, j;
+  // for (i = 0; ebp != 0 && i < STACKFRAME_DEPTH; i++) {
+  //   cprintf("ebp:0x%08x ", ebp);
+  //   cprintf("eip:0x%08x ", eip);
+  //   uint32_t next_ebp = *(uint32_t *)(ebp);
+  //   uint32_t p_arg = ebp + 8;
+
+  //   cprintf("args: ");
+  //   for (j = 0; j < 4; j++) {
+  //     uint32_t arg = *(uint32_t *)(p_arg);
+  //     cprintf("0x%08x ", arg);
+  //     p_arg += 4;
+  //   }
+  //   cprintf("\n ");
+  //   print_debuginfo(eip - 1);
+
+  //   eip = *(uint32_t *)(ebp + 4);
+  //   ebp = next_ebp;
+  // }
+  uint32_t ebp = read_ebp(), eip = read_eip();
+
   int i, j;
   for (i = 0; ebp != 0 && i < STACKFRAME_DEPTH; i++) {
-    cprintf("ebp:0x%08x ", ebp);
-    cprintf("eip:0x%08x ", eip);
-    uint32_t next_ebp = *(uint32_t *)(ebp);
-    uint32_t p_arg = ebp + 8;
-
-    cprintf("args: ");
+    cprintf("ebp:0x%08x eip:0x%08x args:", ebp, eip);
+    uint32_t *args = (uint32_t *)ebp + 2;
     for (j = 0; j < 4; j++) {
-      uint32_t arg = *(uint32_t *)(p_arg);
-      cprintf("0x%08x ", arg);
-      p_arg += 4;
+      cprintf("0x%08x ", args[j]);
     }
-    cprintf("\n ");
+    cprintf("\n");
     print_debuginfo(eip - 1);
-
-    eip = *(uint32_t *)(ebp + 4);
-    ebp = next_ebp;
+    eip = ((uint32_t *)ebp)[1];
+    ebp = ((uint32_t *)ebp)[0];
   }
+}
 }
